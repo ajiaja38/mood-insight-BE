8e03548b4b193b89a233762c0e6c048ae15e8846
ajiaja38 <ajicooljazz38@gmail.com>
2025-07-23 09:02:14 +0700

    feat: update minor :zap:

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Mood Insight!';
+    return 'Hello Mood Insight API!';
   }
 }
